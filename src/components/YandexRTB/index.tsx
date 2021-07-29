41716c99a27e3170815cad285986a8b41ea3a9bf
Kundius Ruslan <kundius.ruslan@gmail.com>
2021-07-29 13:29:34 +0300

    upd

--- src/components/YandexRTB/index.tsx
+++ src/components/YandexRTB/index.tsx
@@ -10,35 +10,42 @@ export const YandexRTB = ({
   horizontalAlign = true
 }: YandexRTBProps) => {
   const ref = useRef<HTMLDivElement | null>(null)
+
+  const elId = Math.random().toString(36).substring(7)
+
   useEffect(() => {
     if (!ref.current) return
 
+    console.log('effect rtb')
+
     const yaDiv = document.createElement('div')
-    yaDiv.setAttribute('id', `yandex_rtb_${id}`)
+    yaDiv.setAttribute('id', `yandex_rtb_${elId}`)
     ref.current.appendChild(yaDiv)
   
     const yaScript = document.createElement('script')
     yaScript.setAttribute('type', 'text/javascript')
-    yaScript.innerHTML = `(function(w, d, n, s, t) {
-          w[n] = w[n] || [];
-          w[n].push(function() {
-              Ya.Context.AdvManager.render({
-                  blockId: "${id}",
-                  renderTo: "yandex_rtb_${id}",
-                  horizontalAlign: ${horizontalAlign ? 'true' : 'false'},
-                  async: true
-              });
-          });
-          t = d.getElementsByTagName("script")[0];
-          s = d.createElement("script");
-          s.type = "text/javascript";
-          s.src = "//an.yandex.ru/system/context.js";
-          s.async = true;
-          t.parentNode.insertBefore(s, t);
-      })(this, this.document, "yandexContextAsyncCallbacks");`
+    yaScript.innerHTML = `
+      (function(w, d, n, s, t) {
+        w[n] = w[n] || [];
+        w[n].push(function() {
+            Ya.Context.AdvManager.render({
+                blockId: "${id}",
+                renderTo: "yandex_rtb_${elId}",
+                horizontalAlign: ${horizontalAlign ? 'true' : 'false'},
+                async: true
+            });
+        });
+        t = d.getElementsByTagName("script")[0];
+        s = d.createElement("script");
+        s.type = "text/javascript";
+        s.src = "//an.yandex.ru/system/context.js";
+        s.async = true;
+        t.parentNode.insertBefore(s, t);
+      })(this, this.document, "yandexContextAsyncCallbacks");
+    `
   
-      document.head.appendChild(yaScript)
-  }, [])
+    ref.current.appendChild(yaScript)
+  })
   return (
     <div ref={ref} />
   )
